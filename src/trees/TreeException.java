d0e2e897516a059fcf9ff2e64b209ee54b255061
Richard Yip <rdyip892@gmail.com>
2023-11-17 14:14:07 -0800

    ...

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -1,9 +1,9 @@
 package trees;
 
-public class TreeException extends ???
+public class TreeException extends Exception
 {
 	public TreeException(String s)
 	{
-		// Pass s into a superclass ctor.
+		super(s);
 	}
 }
